1b8f5af57e8fc8018e3425c2bf32b11d6afe6b6b
ujjawal19 <ujjawalmathur.mathur@gmail.com>
2021-09-08 11:49:47 +0530

    first commit- Build project structure

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Login.css"
+function Login() {
+
+    return (
+        <div className="login">
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="" width="700"/>
+            {/* Spotify Logo */}
+            <button>Login With Spotify</button>
+            {/* Login with spotify button */}
+        </div>
+    )
+}
+
+export default Login
